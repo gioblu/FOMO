a7e6a1c7787dd81a491b1377f9cec354e744bff0
unknown <gioscarab@gmail.com>
2015-06-18 02:39:33 +0200

    Initial commit

---
+++ FOMO.cpp
@@ -0,0 +1,5 @@
+
+ /* FOMO  */
+
+#include "FOMO.h"
+
---
+++ FOMO.h
@@ -0,0 +1,24 @@
+
+ /* FOMO */
+
+#ifndef FOMO_h
+  #define FOMO_h
+
+  #if defined(ARDUINO) && (ARDUINO >= 100)
+    #include "Arduino.h"
+  #else
+    #include "WProgram.h"
+    #include "WConstants.h"
+  #endif
+#endif
+
+class FOMO {
+
+  public:
+
+    FOMO();
+   
+
+  private:
+    
+};
