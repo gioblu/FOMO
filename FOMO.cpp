bc3e285a6e29f4e44edc412166ee04c3092501f7
unknown <gioscarab@gmail.com>
2015-06-25 04:39:41 +0200

    Work is started, not working initial sketchy code

--- FOMO.cpp
+++ FOMO.cpp
@@ -6,7 +6,15 @@
 
 FOMO::FOMO(boolean debug) {
 
+
+  this->add_process(this->update_duration, )
+
 };
+
+/* Add to instance_name.sensors a new sensor
+
+   update_funciton: function that updates sensor value (you should write it by yourself)
+   interval:        time interval between every update_function call */
 
 int FOMO::add_sensor(void (*update_function)(void), unsigned long interval) {
   int id = ID_NOT_AVAILABLE;
@@ -38,7 +46,41 @@ int FOMO::add_sensor(void (*update_function)(void), unsigned long interval) {
   }
 };
 
-int FOMO::disable_sensor(uint8_t id) {
-  this->sensors[id].empty = true;
-}
+/* Remove sensor from instance_name.sensors 
+   (can't be reversed so please understand the implications of this)
 
+   id: the id of the sensor you want to remove */
+
+int FOMO::remove_sensor(uint8_t id) {
+  this->sensors[id].empty = true;
+};
+
+/* Activate sensor update process 
+   (can be deactivated with deactivate_sensor function)
+
+   id: the id of the sensor you want to disable */
+
+
+int FOMO::activate_sensor(uint8_t id) {
+  return this->activate_process(this->sensors[id].process_id);
+};
+
+
+/* Deactivate sensor update process 
+   (can be reactivated with activate_sensor function)
+
+   id: the id of the sensor you want to activate */
+
+int FOMO::deactivate_sensor(uint8_t id) {
+  return this->deactivate_process(this->sensors[id].process_id);
+};
+
+
+int FOMO::report_problem(byte problem) {
+  for(byte i = 0; i < 20; i++)
+    if(this->state->mission->problems[i] == NO_PROBLEM) {
+      this->state->mission->problems[i] = problem;
+      return DONE;
+    }
+  return TOO_MUCH_PROBLEMS;
+};
